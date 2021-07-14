0899c9472a45c346a2cc8278be57ff84becf2b93
Gabriel <gabriel.estudy.reis@gmail.com>
2021-07-14 13:08:48 -0300

    Primeiro Commit
    Primeiro código em Dart

---
+++ dart-studies/hello.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("Iniciando meu primeiro código em Dart!");
+
+  var nome = 'Gabriel';
+  var ano_nascimento = 2001;
+  var planetas = ['Terra', 'Marte', 'Júpiter'];
+  var imagem = {
+    'tags': ['marte'],
+    'url': 'curso-flutter-pacto/imagens'
+  };
+
+  print('Programador: $nome, programando em Dart');
+  print('Ano de nascimento: $ano_nascimento');
+  print('Planetas favoritos: $planetas');
+}
